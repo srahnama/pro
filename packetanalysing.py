0a14e672c47c18b4e70eb39e2921172c186af86f
shahab rahnama <shahaberahnama@gmail.com>
2020-10-24 01:25:59 +0330

    21099

--- packetanalysing.py
+++ packetanalysing.py
@@ -2,12 +2,9 @@ import numpy as np
 import pandas as pd
 import re
 df = pd.read_csv("packets1.csv", sep=",")
-str = '80  >  57244 [ACK] Seq=153969187 Ack=245 Win=14720 Len=1460 [TCP segment of a reassembled PDU]'
-# print(df['Info'][134033])
+
 new_df = df.head(50)
-print( re.search(r"Seq=([0-9]+)", str).group(1))
-print( re.search(r"Len=([0-9]+)", str).group(1))
-print( re.search(r"\[[A-Z]*?\]", str).group())
+
 pd.set_option('display.max_rows', df.shape[0]+1)
 new_df.loc[:, 'Seq'] = None 
 new_df.loc[:, 'Len'] = None 
@@ -22,29 +19,44 @@ for index, row in new_df.iterrows():
     if re.search(r"Len=([0-9]+)", row.Info)  is not None:
         new_df.loc[index,'Len'] = re.search(r"Len=([0-9]+)", row.Info).group(1)
 
-# print(new_df[['Seq','Len']])
+# print(new_df.head(33))
 
 output_df = pd.DataFrame(columns=new_df.columns.tolist())
 
 
 # flags => \[[A-Z]*?\]
 
+# for index, row in new_df.iterrows():
+#     if row.Seq ==None or int(row.Len) == 0:
+# new_df
+#  = new_df.dropna(how='all')
+new_df =new_df.replace(to_replace='None', value=np.nan).dropna()
+print(new_df, new_df.size)
 for index, row in new_df.iterrows():
-    if row.Seq !=None:
-        len = int(row.Len)
+    # if int(row.Len) > 0:
+
+    if index < new_df.shape[0]:
+
+        df_len = int(row.Len)
         # print(row)
         seq = int(row.Seq)
-        next_seq = len + seq
-        output_df = output_df.append(row, ignore_index=True)
-        new_df = new_df.drop(new_df.index[index], axis=0)
+        next_seq = df_len + seq
+        output_df = output_df.append(row)
+        new_df.drop([new_df.index[index]], axis=0, inplace=True)
+        
         for i, r in new_df.iterrows():
-            if r.Seq !=None:
+            # if r.Seq != None and int(r.Len) > 0:
+            if i < new_df.shape[0]:
                 if int(r.Seq) == next_seq:
-                    next_seq = int(r.Seq) + len
-                    output_df = output_df.append(row, ignore_index=True)
-                    new_df = new_df.drop(new_df.index[i], axis=0)
-                    print(output_df)
-    output_df = output_df.append(pd.Series([np.nan]), ignore_index = True)
+                    print(int(r.Seq) , next_seq)
+                    # print(next_seq) 
+                    output_df = output_df.append(r)
+                    # print(new_df[i], new_df.size)
+                    new_df.drop([new_df.index[i]], axis=0, inplace=True)
+                    next_seq = next_seq + df_len
+            # break   
+                    # print(output_df)
+        output_df = output_df.append(pd.Series([np.nan]), ignore_index = True)
 
 
 print(output_df)
